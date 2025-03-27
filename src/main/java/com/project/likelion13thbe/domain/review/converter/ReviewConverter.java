a1d03e4f7b34860ab637b583c95470f64be06bdc
hambining <hyunbini2942@gmail.com>
2025-03-27 22:30:46 +0900

    ✨feat: Add review package

---
+++ src/main/java/com/project/likelion13thbe/domain/review/converter/ReviewConverter.java
@@ -0,0 +1,9 @@
+package com.project.likelion13thbe.domain.review.converter;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ReviewConverter {
+
+}
---
+++ src/main/java/com/project/likelion13thbe/domain/review/dto/request/ReviewRequestDTO.java
@@ -0,0 +1,4 @@
+package com.project.likelion13thbe.domain.review.dto.request;
+
+public record ReviewRequestDTO() {
+}
