b90875a95a1e1a319e21d92ec33e3dc865b62dbb
seohyunseok <tjgustjr33@gmail.com>
2025-03-30 17:15:30 +0900

    feat: 세미나 1주차 실습 과제 완료

---
+++ src/main/java/com/project/likelion13thbe/domain/member/entity/Member.java
@@ -0,0 +1,4 @@
+package com.project.likelion13thbe.domain.member.entity;
+@Entity
+public class Member {
+}
---
+++ src/main/java/com/project/likelion13thbe/domain/member/service/query/MemberQueryServiceImpl.java
@@ -0,0 +1,6 @@
+package com.project.likelion13thbe.domain.member.service.query;
+
+import com.project.likelion13thbe.domain.member.service.command.MemberCommandService;
+
+public class MemberQueryServiceImpl implements MemberCommandService {
+}
