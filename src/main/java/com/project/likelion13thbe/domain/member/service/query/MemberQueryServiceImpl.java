7d018bde68682eab60e2f73e1e150ec4f48af1ee
seohyunseok <tjgustjr33@gmail.com>
2025-03-30 17:18:16 +0900

    feat: 세미나 1주차 실습 과제 완료

--- src/main/java/com/project/likelion13thbe/domain/member/service/query/MemberQueryServiceImpl.java
+++ src/main/java/com/project/likelion13thbe/domain/member/service/query/MemberQueryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.project.likelion13thbe.domain.member.service.query;
 
 import com.project.likelion13thbe.domain.member.service.command.MemberCommandService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberQueryServiceImpl implements MemberCommandService {
 }
