2b2476d4a8c60d36f9d8b321c1bcc3e4d7725cc7
hanmin <rlemdhs2747>
2025-03-31 16:42:59 +0900

    ✨ feat: 세미나 1주차 실습 과제 완료

---
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/response/MemberResDTO.java
@@ -0,0 +1,11 @@
+package com.project.likelion13thbe.domain.member.dto.response;
+
+public class MemberResDTO {
+    public record TestDTO(
+            Long id,
+            String content
+
+    ) {
+
+    }
+}
