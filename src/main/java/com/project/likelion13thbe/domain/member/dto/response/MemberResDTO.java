4bb2cf953a05b8e792550522049e52bd9c353645
ayeon <seoaa3331@gmail.com>
2025-04-01 22:00:55 +0900

    ✨feat: 세미나 1주차 실습

---
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/response/MemberResDTO.java
@@ -0,0 +1,21 @@
+package com.project.likelion13thbe.domain.member.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class MemberResDTO {
+    public record Test1DTO(
+            Long id,
+            String content
+    ){
+    }
+
+    @Getter
+    @Setter
+    public class Test2DTO{
+        private Long id;
+        private String content;
+    }
+
+
+}
