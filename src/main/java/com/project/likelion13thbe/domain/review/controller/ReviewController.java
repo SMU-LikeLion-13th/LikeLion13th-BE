b7c6384394c9316d996761b7aa9a39493d1970ed
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-13 02:05:34 +0900

    ✨feat: 리뷰 단건 조회

--- src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
+++ src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
@@ -1,7 +1,20 @@
 package com.project.likelion13thbe.domain.review.controller;
 
+import com.project.likelion13thbe.domain.review.dto.response.ReviewResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Review", description = "리뷰 관련 API")
 public class ReviewController {
+
+    @Operation(summary = "리뷰 단건 조회")
+    @GetMapping("/api/v1/reviews/{reviewId}")
+    public ResponseEntity<ReviewResponseDTO.ReviewDetailResponseDTO> getReview(@PathVariable Long reviewId) {
+        return null;
+    }
 }
--- src/main/java/com/project/likelion13thbe/domain/review/dto/response/ReviewResponseDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/review/dto/response/ReviewResponseDTO.java
@@ -1,4 +1,19 @@
 package com.project.likelion13thbe.domain.review.dto.response;
 
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
 public class ReviewResponseDTO {
+
+    @Builder
+    public record ReviewDetailResponseDTO(
+            String content,
+            String image,
+            Integer rating,
+            LocalDateTime createdAt,
+            String nickname,
+            String profileImage
+    ) {
+    }
 }
