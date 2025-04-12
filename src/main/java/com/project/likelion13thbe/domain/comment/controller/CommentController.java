a0d0d4f2e0edd659e4c21aad96dc561ff646cdb8
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-13 03:59:45 +0900

    ✨feat: 댓글 삭제

--- src/main/java/com/project/likelion13thbe/domain/comment/controller/CommentController.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/controller/CommentController.java
@@ -59,4 +59,16 @@ public class CommentController {
     public ResponseEntity<?> editComment(@PathVariable String commentId) {
         return null;
     }
+
+    @Operation(summary = "댓글 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "No Content",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "NotFound",
+                    content = @Content(mediaType = "application/json"))
+    })
+    @DeleteMapping("/api/v1/comments/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable Long commentId) {
+        return null;
+    }
 }
