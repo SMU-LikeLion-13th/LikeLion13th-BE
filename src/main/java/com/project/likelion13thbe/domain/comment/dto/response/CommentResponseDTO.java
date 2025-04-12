df834beb1e411258802fb8ebf88f5471040b35f2
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-13 03:03:19 +0900

    ✨feat: add comment package

---
+++ src/main/java/com/project/likelion13thbe/domain/comment/dto/response/CommentResponseDTO.java
@@ -0,0 +1,4 @@
+package com.project.likelion13thbe.domain.comment.dto.response;
+
+public class CommentResponseDTO {
+}
---
+++ src/main/java/com/project/likelion13thbe/domain/comment/service/command/CommentCommandService.java
@@ -0,0 +1,4 @@
+package com.project.likelion13thbe.domain.comment.service.command;
+
+public interface CommentCommandService {
+}
