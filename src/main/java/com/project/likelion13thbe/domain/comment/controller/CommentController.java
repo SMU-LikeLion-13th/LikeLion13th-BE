23d860dd8c33fc5156bcbc2f1da616a776769129
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-13 03:48:16 +0900

    ✨feat: 댓글 목록 조회

--- src/main/java/com/project/likelion13thbe/domain/comment/controller/CommentController.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/controller/CommentController.java
@@ -1,7 +1,32 @@
 package com.project.likelion13thbe.domain.comment.controller;
 
+import com.project.likelion13thbe.domain.comment.dto.response.CommentResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Comment", description = "댓글 관련 API")
 public class CommentController {
+
+    @Operation(summary = "댓글 목록 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommentResponseDTO.CommentListResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "NotFound",
+                    content = @Content(mediaType = "application/json"))
+    })
+    @GetMapping("/api/v1/products/{productId}/reviews/{reviewId}/comments")
+    public ResponseEntity<CommentResponseDTO.CommentListResponseDTO> getCommentList(
+            @PathVariable Long productId, @PathVariable Long reviewId
+    ) {
+        return null;
+    }
 }
--- src/main/java/com/project/likelion13thbe/domain/comment/dto/response/CommentResponseDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/dto/response/CommentResponseDTO.java
@@ -1,4 +1,23 @@
 package com.project.likelion13thbe.domain.comment.dto.response;
 
+import lombok.Builder;
+
+import java.util.List;
+
 public class CommentResponseDTO {
+
+    @Builder
+    public record CommentDetailResponseDTO(
+            Long commentId,
+            String content,
+            String nickname,
+            Integer likeCount
+    ) {
+    }
+
+    @Builder
+    public record CommentListResponseDTO(
+            List<CommentDetailResponseDTO> commentList
+    ) {
+    }
 }
