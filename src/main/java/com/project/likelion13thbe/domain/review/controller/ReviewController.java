3e341cda199cc1e7c5ec6936c4ddb0549643aeec
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-13 02:54:15 +0900

    ♻️refactor: 리뷰 조회 @ApiResponses 추가

--- src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
+++ src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
@@ -16,12 +16,24 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
 
     @Operation(summary = "리뷰 단건 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewResponseDTO.ReviewDetailResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "NotFound",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/api/v1/reviews/{reviewId}")
     public ResponseEntity<ReviewResponseDTO.ReviewDetailResponseDTO> getReview(@PathVariable Long reviewId) {
         return null;
     }
 
     @Operation(summary = "리뷰 목록 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewResponseDTO.ReviewListResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "NotFound",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/api/v1/products/{productId}/reviews")
     public ResponseEntity<ReviewResponseDTO.ReviewListResponseDTO> getReviewList(@PathVariable Long productId) {
         return null;
