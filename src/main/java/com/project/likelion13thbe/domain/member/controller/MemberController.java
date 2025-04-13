6370be5b6bf7855078f14057b2da7beae6b42fab
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-14 00:40:16 +0900

    ✨feat: 일반 로그인

--- src/main/java/com/project/likelion13thbe/domain/member/controller/MemberController.java
+++ src/main/java/com/project/likelion13thbe/domain/member/controller/MemberController.java
@@ -1,7 +1,31 @@
 package com.project.likelion13thbe.domain.member.controller;
 
+import com.project.likelion13thbe.domain.member.dto.request.MemberRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Member", description = "유저 관련 API")
 public class MemberController {
+
+    @Operation(summary = "일반 로그인")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "BadRequest",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "Unauthorized\t\n 1. 가입되지 않은 회원 \t\n 2. 아이디나 비밀번호 오류",
+                    content = @Content(mediaType = "application/json"))
+    })
+    @PostMapping("/api/v1/login")
+    public ResponseEntity<?> localLogin(@RequestBody MemberRequestDTO.loginRequestDTO loginRequestDTO) {
+        return null;
+    }
 }
--- src/main/java/com/project/likelion13thbe/domain/member/dto/request/MemberRequestDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/request/MemberRequestDTO.java
@@ -1,4 +1,12 @@
 package com.project.likelion13thbe.domain.member.dto.request;
 
+import lombok.Builder;
+
 public class MemberRequestDTO {
+
+    @Builder
+    public record loginRequestDTO(
+            String email,
+            String password) {
+    }
 }
