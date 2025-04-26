371fcab68a944f497fda9d6473eb12c6b5b19e9b
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-26 16:30:19 +0900

    ♻️refactor: Add @Builder annotations to DTOs

--- src/main/java/com/project/likelion13thbe/domain/member/dto/request/MemberRequestDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/request/MemberRequestDTO.java
@@ -26,6 +26,7 @@ public class MemberRequestDTO {
     ) {
     }
 
+    @Builder
     public record kakaoLoginRequestDTO(
             String authorizationCode
     ) {
--- src/main/java/com/project/likelion13thbe/domain/member/dto/response/MemberResponseDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/response/MemberResponseDTO.java
@@ -1,6 +1,9 @@
 package com.project.likelion13thbe.domain.member.dto.response;
 
+import lombok.Builder;
+
 public class MemberResponseDTO {
+    @Builder
     public record JwtTokenResponse(
             String accessToken,
             String refreshToken,
