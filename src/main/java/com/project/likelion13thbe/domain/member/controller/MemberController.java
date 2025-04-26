64e769fe9f491eef28ef5bebf32afe126b08d4e3
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-04-26 15:57:14 +0900

    ♻️refactor: 일반 로그인 수정

--- src/main/java/com/project/likelion13thbe/domain/member/controller/MemberController.java
+++ src/main/java/com/project/likelion13thbe/domain/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.project.likelion13thbe.domain.member.controller;
 
 import com.project.likelion13thbe.domain.member.dto.request.MemberRequestDTO;
+import com.project.likelion13thbe.domain.member.dto.response.MemberResponseDTO;
 import com.project.likelion13thbe.domain.review.dto.response.ReviewResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -21,14 +22,15 @@ public class MemberController {
     @Operation(summary = "일반 로그인")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(mediaType = "application/json")),
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberResponseDTO.JwtTokenResponse.class))),
             @ApiResponse(responseCode = "400", description = "BadRequest",
                     content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "401", description = "Unauthorized\t\n 1. 가입되지 않은 회원 \t\n 2. 아이디나 비밀번호 오류",
+            @ApiResponse(responseCode = "401", description = "Unauthorized 아이디나 비밀번호 오류",
                     content = @Content(mediaType = "application/json"))
     })
     @PostMapping("/api/v1/login")
-    public ResponseEntity<?> localLogin(@RequestBody MemberRequestDTO.LoginRequestDTO loginRequestDTO) {
+    public ResponseEntity<MemberResponseDTO.JwtTokenResponse> localLogin(@RequestBody MemberRequestDTO.LoginRequestDTO loginRequestDTO) {
         return null;
     }
 
--- src/main/java/com/project/likelion13thbe/domain/member/dto/response/MemberResponseDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/response/MemberResponseDTO.java
@@ -1,4 +1,10 @@
 package com.project.likelion13thbe.domain.member.dto.response;
 
 public class MemberResponseDTO {
+    public record JwtTokenResponse(
+            String accessToken,
+            String refreshToken,
+            Long expiresIn
+    ) {
+    }
 }
