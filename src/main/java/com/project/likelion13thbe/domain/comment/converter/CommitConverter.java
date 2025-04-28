7074ba5b52cefcce2cad15740a4f9d434055ec8a
seohyunseok <tjgustjr33@gmail.com>
2025-04-28 18:54:02 +0900

    ✨feat: Add commit package

---
+++ src/main/java/com/project/likelion13thbe/domain/comment/converter/CommitConverter.java
@@ -0,0 +1,8 @@
+package com.project.likelion13thbe.domain.comment.converter;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommitConverter {
+}
