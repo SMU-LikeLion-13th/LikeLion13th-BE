2442b48a07ad623e9382b37b3b2de7f8bfdfb345
Gyueon Lee <gon2yq@naver.com>
2025-04-28 15:58:31 +0900

    ✨feat: 리뷰 세부 조회

--- src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
+++ src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
@@ -1,7 +1,32 @@
 package com.project.likelion13thbe.domain.review.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.likelion13thbe.domain.review.dto.request.ReviewReqDTO;
+import com.project.likelion13thbe.domain.review.dto.response.ReviewResDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@Tag(name = "Review", description = "리뷰 관련 API")
 public class ReviewController {
+
+    @Operation(summary = "리뷰 세부 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ReviewResDTO.ReviewDetailResDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content(mediaType = "application/json"))
+    })
+    @GetMapping("/api/v1/reviews/{reviewId}")
+    public ResponseEntity<ReviewResDTO.ReviewDetailResDTO> getReview(@PathVariable Long reviewId) {
+        return null;
+    }
+
 }
--- src/main/java/com/project/likelion13thbe/domain/review/dto/response/ReviewResDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/review/dto/response/ReviewResDTO.java
@@ -1,4 +1,23 @@
 package com.project.likelion13thbe.domain.review.dto.response;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
 public class ReviewResDTO {
+
+    @Builder
+    public record ReviewDetailResDTO(
+            Long reviewId,
+            Double rating,
+            String content,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt,
+            String nickname,
+            String profileImage
+    ) {
+    }
+
 }
