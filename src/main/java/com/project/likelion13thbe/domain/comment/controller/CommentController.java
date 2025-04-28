be6185b07290897edb89675ca1db3c560b91b6ac
Gyueon Lee <gon2yq@naver.com>
2025-04-28 16:09:56 +0900

    ✨feat: 댓글 목록 조회

--- src/main/java/com/project/likelion13thbe/domain/comment/controller/CommentController.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/controller/CommentController.java
@@ -1,7 +1,32 @@
 package com.project.likelion13thbe.domain.comment.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.likelion13thbe.domain.comment.dto.request.CommentReqDTO;
+import com.project.likelion13thbe.domain.comment.dto.response.CommentResDTO;
+import com.project.likelion13thbe.domain.review.dto.response.ReviewResDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Comment", description = "댓글 관련 API")
 public class CommentController {
+
+    @Operation(summary = "댓글 목록 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CommentResDTO.CommentListResDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content(mediaType = "application/json"))
+    })
+    @GetMapping("/api/v1/reviews/{reviewId}/comments")
+    public ResponseEntity<ReviewResDTO.ReviewListResDTO> getComment(@PathVariable Long reviewId) {
+        return null;
+    }
+
 }
