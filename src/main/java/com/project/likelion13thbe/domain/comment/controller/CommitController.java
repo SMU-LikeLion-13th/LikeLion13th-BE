139c17b0ab396a1daa60479503e6ad1665eb7a55
seohyunseok <tjgustjr33@gmail.com>
2025-04-28 19:09:36 +0900

    ✨feat: 댓글 작성

--- src/main/java/com/project/likelion13thbe/domain/comment/controller/CommitController.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/controller/CommitController.java
@@ -1,13 +1,14 @@
 package com.project.likelion13thbe.domain.comment.controller;
 
+import com.project.likelion13thbe.domain.comment.dto.request.CommitReqDTO;
 import com.project.likelion13thbe.domain.comment.dto.response.CommitResDTO;
+import com.project.likelion13thbe.domain.review.dto.request.ReviewReqDTO;
+import com.project.likelion13thbe.domain.review.dto.response.ReviewResDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name="Commit", description = "댓글 관련 API")
@@ -17,6 +18,18 @@ public class CommitController {
     @Parameter(name = "reviewId", description = "review PK", example = "1")
     @GetMapping("/api/v1/products/{productId}/reviews/{reviewId}/commits")
     public ResponseEntity<CommitResDTO.CommentListResDTO> getCommits(@PathVariable Long productId, @PathVariable Long reviewId) {
+        return ResponseEntity.ok(null);
+    }
+
+    @Operation(description = "댓글 작성")
+    @Parameter(name = "productId", description = "product PK", example = "1")
+    @Parameter(name = "reviewId", description = "review PK", example = "1")
+    @GetMapping("/api/v1/products/{productId}/reviews/{reviewId}/commits")
+    public ResponseEntity<CommitResDTO.CommentResDTO> createCommit(
+            @PathVariable Long productId,
+            @PathVariable Long reviewId,
+            @RequestBody CommitReqDTO.CreateCommitDTO createCommitDTO
+    ) {
         return ResponseEntity.ok(null);
     }
 }
--- src/main/java/com/project/likelion13thbe/domain/comment/dto/request/CommitReqDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/dto/request/CommitReqDTO.java
@@ -1,4 +1,11 @@
 package com.project.likelion13thbe.domain.comment.dto.request;
 
+import lombok.Builder;
+
 public class CommitReqDTO {
+    @Builder
+    public record CreateCommitDTO(
+            String content
+    ) {
+    }
 }
