fcd602d7358d4b6a0117685a581026e7b3010951
seohyunseok <tjgustjr33@gmail.com>
2025-04-28 19:05:07 +0900

    ✨feat: 댓글 목록 조회

--- src/main/java/com/project/likelion13thbe/domain/comment/controller/CommitController.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/controller/CommitController.java
@@ -1,7 +1,22 @@
 package com.project.likelion13thbe.domain.comment.controller;
 
+import com.project.likelion13thbe.domain.comment.dto.response.CommitResDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name="Commit", description = "댓글 관련 API")
 public class CommitController {
+    @Operation(description = "댓글 목록 조회")
+    @Parameter(name = "productId", description = "product PK", example = "1")
+    @Parameter(name = "reviewId", description = "review PK", example = "1")
+    @GetMapping("/api/v1/products/{productId}/reviews/{reviewId}/commits")
+    public ResponseEntity<CommitResDTO.CommentListResDTO> getCommits(@PathVariable Long productId, @PathVariable Long reviewId) {
+        return ResponseEntity.ok(null);
+    }
 }
