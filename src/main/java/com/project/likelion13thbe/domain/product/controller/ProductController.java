a62d9a5c58f344b4375d0b6f41ce403953afc28c
Gyueon Lee <gon2yq@naver.com>
2025-04-28 16:05:55 +0900

    ✨feat: 상품 상세 조회

--- src/main/java/com/project/likelion13thbe/domain/product/controller/ProductController.java
+++ src/main/java/com/project/likelion13thbe/domain/product/controller/ProductController.java
@@ -1,7 +1,31 @@
 package com.project.likelion13thbe.domain.product.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.likelion13thbe.domain.product.dto.request.ProductReqDTO;
+import com.project.likelion13thbe.domain.product.dto.response.ProductResDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Product", description = "상품 관련 API")
 public class ProductController {
+
+    @Operation(summary = "상품 상세 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProductResDTO.ProductDetailResDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content(mediaType = "application/json"))
+    })
+    @GetMapping("/api/v1/product/{productId}")
+    public ResponseEntity<ProductResDTO.ProductDetailResDTO> getProduct(@PathVariable Long productId) {
+        return null;
+    }
+
 }
