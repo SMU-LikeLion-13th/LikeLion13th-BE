3a3202623d6a8e1a4f8d2cb18c8a6ac01f4afd92
tuna1025 <jo215098@gmail.com>
2025-04-30 02:44:54 +0900

    ✨feat: 세미나 3주차 리뷰 컨트롤러 구현완료

--- src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
+++ src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
@@ -1,7 +1,51 @@
 package com.project.likelion13thbe.domain.review.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.likelion13thbe.domain.review.dto.request.ReviewReqDTO;
+import com.project.likelion13thbe.domain.review.dto.response.ReviewResDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
+@Tag(name="Review",description = "리뷰 API")
 public class ReviewController {
+
+    @Operation(summary = "리뷰단일조회")
+    @GetMapping("/reviews/{reviewId}")
+    public ReviewResDTO.ReviewResponseDTO getReview(@PathVariable Long reviewId) {
+        return null;
+    }
+
+    @Operation(summary = "리뷰 목록 조회")
+    @GetMapping("/products/{productid}/reviews")
+    public  ReviewResDTO.ReviewListResponseDTO getReviewsList(@PathVariable Long productId) {
+        return null;
+    }
+
+    @Operation(summary = "내 리뷰 조회")
+    @GetMapping("/users/{userId}/reviews")
+    public ReviewResDTO.ReviewListResponseDTO getMyReviewsList(@PathVariable Long userId) {
+        return null;
+    }
+
+    @Operation(summary = "리뷰 작성")
+    @PostMapping("/product/{productId}/reviews/{reviewId}")
+    public ReviewResDTO.ReviewCreateDTO PostReview(@PathVariable Long productId ) {
+        return null;
+    }
+
+    @Operation(summary = "리뷰 삭제")
+    @DeleteMapping("/reviews/{reviewId}")
+    public ReviewResDTO.ReviewResponseDTO getReviews(@PathVariable Long reviewId) {
+        return null;
+    }
+
+    @Operation(summary = "리뷰 수정")
+    @PatchMapping("/reviews/{reviewId}")
+    public ReviewResDTO.ReviewUpdateDTO PatchReviewUpdate(@PathVariable Long reviewId) {
+        return null;
+    }
+
+
 }
