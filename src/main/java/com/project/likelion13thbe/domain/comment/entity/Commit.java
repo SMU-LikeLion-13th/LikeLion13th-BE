abf0c52f347bd8aee21d861ad06bafde70f00dbc
seohyunseok <tjgustjr33@gmail.com>
2025-05-01 23:24:20 +0900

    ✨feat: Commit 엔티티 생성

--- src/main/java/com/project/likelion13thbe/domain/comment/entity/Commit.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/entity/Commit.java
@@ -1,4 +1,35 @@
 package com.project.likelion13thbe.domain.comment.entity;
 
-public class Commit {
+import com.project.likelion13thbe.domain.member.entity.Member;
+import com.project.likelion13thbe.domain.product.entity.Product;
+import com.project.likelion13thbe.domain.review.entity.Review;
+import com.project.likelion13thbe.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Commit extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
 }
