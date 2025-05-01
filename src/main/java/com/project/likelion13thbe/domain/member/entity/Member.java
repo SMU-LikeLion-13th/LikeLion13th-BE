fc3771997c626cb5144ee18eae2e67dca25667bb
Gyueon Lee <gon2yq@naver.com>
2025-05-01 16:33:21 +0900

    ✨feat: Member entity 설계 (실습)

--- src/main/java/com/project/likelion13thbe/domain/member/entity/Member.java
+++ src/main/java/com/project/likelion13thbe/domain/member/entity/Member.java
@@ -1,5 +1,32 @@
 package com.project.likelion13thbe.domain.member.entity;
 
-//@Entity
-public class Member {
+import com.project.likelion13thbe.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "member")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
 }
