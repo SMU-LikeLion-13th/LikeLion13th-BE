08f5b1251ab8b2cb32ef7e4611a39020c38fb807
seohyunseok <tjgustjr33@gmail.com>
2025-05-01 23:23:32 +0900

    ✨feat: Product 엔티티 생성

--- src/main/java/com/project/likelion13thbe/domain/product/entity/Product.java
+++ src/main/java/com/project/likelion13thbe/domain/product/entity/Product.java
@@ -1,4 +1,36 @@
 package com.project.likelion13thbe.domain.product.entity;
-/*@Entity*/
-public class Product {
+
+import com.project.likelion13thbe.domain.member.entity.Member;
+import com.project.likelion13thbe.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
 }
