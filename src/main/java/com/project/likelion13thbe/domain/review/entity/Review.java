d5a56f7c77512c14baff078b16225c5ad402f8f5
seohyunseok <tjgustjr33@gmail.com>
2025-05-01 23:23:42 +0900

    ✨feat: Review 엔티티 생성

--- src/main/java/com/project/likelion13thbe/domain/review/entity/Review.java
+++ src/main/java/com/project/likelion13thbe/domain/review/entity/Review.java
@@ -1,4 +1,33 @@
 package com.project.likelion13thbe.domain.review.entity;
-/*@Entity*/
-public class Review {
+
+import com.project.likelion13thbe.domain.member.entity.Member;
+import com.project.likelion13thbe.domain.product.entity.Product;
+import com.project.likelion13thbe.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "rate", nullable = false)
+    private double rate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
 }
