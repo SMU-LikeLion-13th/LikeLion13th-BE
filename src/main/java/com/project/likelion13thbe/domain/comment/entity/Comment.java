833aea89c837045adfd18ff1a2ced2549da3d1e2
Gyueon Lee <gon2yq@naver.com>
2025-05-02 21:01:32 +0900

    ✨feat: Comment entity 설계

--- src/main/java/com/project/likelion13thbe/domain/comment/entity/Comment.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/entity/Comment.java
@@ -1,5 +1,30 @@
 package com.project.likelion13thbe.domain.comment.entity;
 
-//@Entity
-public class Comment {
+import com.project.likelion13thbe.domain.member.entity.Member;
+import com.project.likelion13thbe.domain.review.entity.Review;
+import com.project.likelion13thbe.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
 }
