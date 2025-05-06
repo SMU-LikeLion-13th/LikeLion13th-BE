e323381c79c194fbd91ea4d636acc3e0d589c3d0
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-05-06 20:59:51 +0900

    ✨feat: 댓글 엔티티 생성

--- src/main/java/com/project/likelion13thbe/domain/comment/entity/Comment.java
+++ src/main/java/com/project/likelion13thbe/domain/comment/entity/Comment.java
@@ -1,5 +1,38 @@
 package com.project.likelion13thbe.domain.comment.entity;
 
-//@Entity
-public class Comment {
+import com.project.likelion13thbe.domain.member.entity.Member;
+import com.project.likelion13thbe.domain.review.entity.Review;
+import com.project.likelion13thbe.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
+public class Comment extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(nullable = false)
+    private int likeCount;
 }
