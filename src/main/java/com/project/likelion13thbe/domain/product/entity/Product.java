6409876e39693dcec0bc4374714003cc3b09af29
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-05-06 20:59:34 +0900

    ✨feat: 상품 엔티티 생성

--- src/main/java/com/project/likelion13thbe/domain/product/entity/Product.java
+++ src/main/java/com/project/likelion13thbe/domain/product/entity/Product.java
@@ -1,6 +1,56 @@
 package com.project.likelion13thbe.domain.product.entity;
 
-//@Entity
-public class Product {
+import com.project.likelion13thbe.domain.member.entity.Member;
+import com.project.likelion13thbe.domain.review.entity.Review;
+import com.project.likelion13thbe.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
+public class Product extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private double ratingAvg;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType productType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "product")
+    private List<Review> reviews = new ArrayList<>();
 
 }
