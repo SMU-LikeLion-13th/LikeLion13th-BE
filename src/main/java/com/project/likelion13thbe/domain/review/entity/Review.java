918dd7bdecc4c1ba1426ae48a26ea5ee68025b35
tuna1025 <jo215098@gmail.com>
2025-05-06 20:44:56 +0900

    ✨feat: Member entity 생성

--- src/main/java/com/project/likelion13thbe/domain/review/entity/Review.java
+++ src/main/java/com/project/likelion13thbe/domain/review/entity/Review.java
@@ -1,4 +1,35 @@
 package com.project.likelion13thbe.domain.review.entity;
 
-public class Review {
+import com.project.likelion13thbe.domain.member.entity.Member;
+import com.project.likelion13thbe.domain.product.entity.Product;
+import com.project.likelion13thbe.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "Score", nullable = false)
+    private Double score;
+
+    @Column(name = "image")
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product; //리뷰 작성한 상품
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;  //리뷰 작성자
 }
