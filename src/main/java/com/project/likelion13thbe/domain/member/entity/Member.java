6ff041880a18941cebe6746d523ab22a4139f5f8
seoaaa <seoaa3331@gmail.com>
2025-05-06 22:58:06 +0900

    ✨feat: Member 엔티티 생성

--- src/main/java/com/project/likelion13thbe/domain/member/entity/Member.java
+++ src/main/java/com/project/likelion13thbe/domain/member/entity/Member.java
@@ -1,4 +1,31 @@
 package com.project.likelion13thbe.domain.member.entity;
 
-public class Member {
+import com.project.likelion13thbe.global.entity.BaseEntity;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Tag(name="member")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(name="image")
+    private String image;
+
 }
