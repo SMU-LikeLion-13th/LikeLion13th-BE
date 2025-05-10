7f83cfca3713b8aad6fa1b6be237b65007083300
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-05-10 17:13:32 +0900

    ✨feat: MemberErrorCode 추가

---
+++ src/main/java/com/project/likelion13thbe/domain/member/exception/MemberErrorCode.java
@@ -0,0 +1,18 @@
+package com.project.likelion13thbe.domain.member.exception;
+
+import com.project.likelion13thbe.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorCode implements BaseErrorCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404_1", "회원을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+}
