c47f38b69e3b87b07357cc2a5eae8e7b1483e164
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-05-10 19:27:28 +0900

    ✨feat: CommentErrorCode & CommentException 추가

---
+++ src/main/java/com/project/likelion13thbe/domain/comment/exception/CommentException.java
@@ -0,0 +1,9 @@
+package com.project.likelion13thbe.domain.comment.exception;
+
+import com.project.likelion13thbe.global.apiPayload.exception.CustomException;
+
+public class CommentException extends CustomException {
+    public CommentException(CommentErrorCode errorCode) {
+        super(errorCode);
+    }
+}
