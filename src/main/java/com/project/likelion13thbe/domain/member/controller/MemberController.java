63bb0552bcccb95379ca45b5d19f36b0140d5e98
Gyueon Lee <gon2yq@naver.com>
2025-05-12 16:56:03 +0900

    ♻️refactor: 사용하지 않는 MemberAPI 잠시 주석... (Swagger 불편 이슈)

--- src/main/java/com/project/likelion13thbe/domain/member/controller/MemberController.java
+++ src/main/java/com/project/likelion13thbe/domain/member/controller/MemberController.java
@@ -73,63 +73,63 @@ public class MemberController {
     ) {
         return ResponseEntity.ok(memberQueryService.getMemberCursor(cursor, size));
     }
-
-    @Operation(summary = "비밀번호 수정")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "401", description = "Unauthorized",
-                    content = @Content(mediaType = "application/json"))
-    })
-    @PostMapping("password-reset")
-    public ResponseEntity<?> resetPassword(@RequestBody MemberReqDTO.ResetPasswordReqDTO resetPasswordReqDTO) {
-        return null;
-    }
-
-    @Operation(summary = "회원 가입")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "409", description = "Conflict",
-                    content = @Content(mediaType = "application/json"))
-    })
-    @PostMapping("sign-up")
-    public ResponseEntity<?> signUp(@RequestBody MemberReqDTO.SignUpResDTO signUpResDTO) {
-        return null;
-    }
-
-    @Operation(summary = "로그인")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = MemberResDTO.LoginJwtTokenResDTO.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized",
-                    content = @Content(mediaType = "application/json"))
-    })
-    @PostMapping("login")
-    public ResponseEntity<MemberResDTO.LoginJwtTokenResDTO> login(@RequestBody MemberReqDTO.LoginResDTO LoginResDTO) {
-        return null;
-    }
-
-    @Operation(summary = "카카오 로그인")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = MemberResDTO.LoginJwtTokenResDTO.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized",
-                    content = @Content(mediaType = "application/json"))
-    })
-    @PostMapping("login/kakao")
-    public ResponseEntity<MemberResDTO.LoginJwtTokenResDTO> kakaoLogin(@RequestBody MemberReqDTO.KakaoLoginResDTO KakaoLoginResDTO) {
-        // 프론트가 카카오에게 받은 인가코드를 Req에 넣어서 백엔드에 전달하면
-        // 백엔드가 카카오에서 토큰을 받아오고
-        // 자체적인 서비스 전용 jwt를 만들어서 Res로 보내주는 방식이긴 합니다만, 아직까지도 아리송합니다
-        // 그리고 토큰을 ResBody에 보내면 xss같은 탈취 보안 이슈가 있어 ResHead로 보내야 한다고는 합니다만...
-        // 능력 부족 이슈로 카카오는 뭔가 이해가 어렵네요
-        return null;
-    }
+//
+//    @Operation(summary = "비밀번호 수정")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "OK",
+//                    content = @Content(mediaType = "application/json")),
+//            @ApiResponse(responseCode = "400", description = "Bad Request",
+//                    content = @Content(mediaType = "application/json")),
+//            @ApiResponse(responseCode = "401", description = "Unauthorized",
+//                    content = @Content(mediaType = "application/json"))
+//    })
+//    @PostMapping("password-reset")
+//    public ResponseEntity<?> resetPassword(@RequestBody MemberReqDTO.ResetPasswordReqDTO resetPasswordReqDTO) {
+//        return null;
+//    }
+//
+//    @Operation(summary = "회원 가입")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "OK",
+//                    content = @Content(mediaType = "application/json")),
+//            @ApiResponse(responseCode = "400", description = "Bad Request",
+//                    content = @Content(mediaType = "application/json")),
+//            @ApiResponse(responseCode = "409", description = "Conflict",
+//                    content = @Content(mediaType = "application/json"))
+//    })
+//    @PostMapping("sign-up")
+//    public ResponseEntity<?> signUp(@RequestBody MemberReqDTO.SignUpResDTO signUpResDTO) {
+//        return null;
+//    }
+//
+//    @Operation(summary = "로그인")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "OK",
+//                    content = @Content(mediaType = "application/json",
+//                            schema = @Schema(implementation = MemberResDTO.LoginJwtTokenResDTO.class))),
+//            @ApiResponse(responseCode = "401", description = "Unauthorized",
+//                    content = @Content(mediaType = "application/json"))
+//    })
+//    @PostMapping("login")
+//    public ResponseEntity<MemberResDTO.LoginJwtTokenResDTO> login(@RequestBody MemberReqDTO.LoginResDTO LoginResDTO) {
+//        return null;
+//    }
+//
+//    @Operation(summary = "카카오 로그인")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "OK",
+//                    content = @Content(mediaType = "application/json",
+//                            schema = @Schema(implementation = MemberResDTO.LoginJwtTokenResDTO.class))),
+//            @ApiResponse(responseCode = "401", description = "Unauthorized",
+//                    content = @Content(mediaType = "application/json"))
+//    })
+//    @PostMapping("login/kakao")
+//    public ResponseEntity<MemberResDTO.LoginJwtTokenResDTO> kakaoLogin(@RequestBody MemberReqDTO.KakaoLoginResDTO KakaoLoginResDTO) {
+//        // 프론트가 카카오에게 받은 인가코드를 Req에 넣어서 백엔드에 전달하면
+//        // 백엔드가 카카오에서 토큰을 받아오고
+//        // 자체적인 서비스 전용 jwt를 만들어서 Res로 보내주는 방식이긴 합니다만, 아직까지도 아리송합니다
+//        // 그리고 토큰을 ResBody에 보내면 xss같은 탈취 보안 이슈가 있어 ResHead로 보내야 한다고는 합니다만...
+//        // 능력 부족 이슈로 카카오는 뭔가 이해가 어렵네요
+//        return null;
+//    }
 }
