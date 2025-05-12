c064f905254ddc0c9a9c81df99d733d2177e5ebe
Gyueon Lee <gon2yq@naver.com>
2025-05-12 12:59:25 +0900

    🔥️remove: Review ApiResponses 제거

--- src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
+++ src/main/java/com/project/likelion13thbe/domain/review/controller/ReviewController.java
@@ -24,40 +24,18 @@ public class ReviewController {
     private final ReviewQueryService reviewQueryService;
 
     @Operation(summary = "리뷰 세부 조회")
-//    @ApiResponses({
-//            @ApiResponse(responseCode = "200", description = "OK",
-//                    content = @Content(mediaType = "application/json",
-//                            schema = @Schema(implementation = ReviewResDTO.ReviewDetailResDTO.class))),
-//            @ApiResponse(responseCode = "404", description = "Not Found",
-//                    content = @Content(mediaType = "application/json"))
-//    })
     @GetMapping("reviews/{reviewId}")
     public ResponseEntity<ReviewResDTO.ReviewDetailResDTO> getReview(@PathVariable Long reviewId) {
         return ResponseEntity.ok(reviewQueryService.getReview(reviewId));
     }
 
     @Operation(summary = "리뷰 목록 조회")
-//    @ApiResponses({
-//            @ApiResponse(responseCode = "200", description = "OK",
-//                    content = @Content(mediaType = "application/json",
-//                            schema = @Schema(implementation = ReviewResDTO.ReviewListResDTO.class))),
-//            @ApiResponse(responseCode = "404", description = "Not Found",
-//                    content = @Content(mediaType = "application/json"))
-//    })
     @GetMapping("products/{productId}/reviews")
     public ResponseEntity<ReviewResDTO.ReviewListResDTO> getReviewList(@PathVariable Long productId) {
         return ResponseEntity.ok(reviewQueryService.getReviewList(productId));
     }
 
     @Operation(summary = "리뷰 작성")
-//    @ApiResponses({
-//            @ApiResponse(responseCode = "201", description = "Created",
-//                    content = @Content(mediaType = "application/json")),
-//            @ApiResponse(responseCode = "400", description = "Bad Request",
-//                    content = @Content(mediaType = "application/json")),
-//            @ApiResponse(responseCode = "404", description = "Not Found",
-//                    content = @Content(mediaType = "application/json"))
-//    })
     @PostMapping("products/{productId}/reviews")
     public ResponseEntity<ReviewResDTO.ReviewCreateResDTO> createReview(
             @PathVariable Long productId, @RequestBody ReviewReqDTO.ReviewCreateReqDTO reviewCreateReqDTO) {
@@ -67,39 +45,18 @@ public class ReviewController {
     }
 
     @Operation(summary = "리뷰 수정")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "404", description = "Not Found",
-                    content = @Content(mediaType = "application/json"))
-    })
     @PatchMapping("reviews/{reviewId}")
     public ResponseEntity<?> patchReview(@PathVariable Long reviewId, @RequestBody ReviewReqDTO.ReviewUpdateReqDTO reviewUpdateReqDTO) {
         return null;
     }
 
     @Operation(summary = "리뷰 삭제")
-    @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "No Content",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "404", description = "Not Found",
-                    content = @Content(mediaType = "application/json"))
-    })
     @DeleteMapping("reviews/{reviewId}")
     public ResponseEntity<?> deleteReview(@PathVariable Long reviewId) {
         return null;
     }
 
     @Operation(summary = "내 리뷰 조회")
-//    @ApiResponses({
-//            @ApiResponse(responseCode = "200", description = "OK",
-//                    content = @Content(mediaType = "application/json",
-//                            schema = @Schema(implementation = ReviewResDTO.ReviewListResDTO.class))),
-//            @ApiResponse(responseCode = "401", description = "Unauthorized",
-//                    content = @Content(mediaType = "application/json"))
-//    })
     @GetMapping("/reviews/my")
     public ResponseEntity<ReviewResDTO.ReviewListResDTO> getMyReviews() {
         return ResponseEntity.ok(reviewQueryService.getMyReviewList());
