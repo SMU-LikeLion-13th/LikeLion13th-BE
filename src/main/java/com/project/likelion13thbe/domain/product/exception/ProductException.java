5cb527cf5462679ed0c4b10faacb06083b15ad97
seoaaa <seoaa3331@gmail.com>
2025-05-13 19:51:47 +0900

    ✨feat: 예외처리 구현

---
+++ src/main/java/com/project/likelion13thbe/domain/product/exception/ProductException.java
@@ -0,0 +1,9 @@
+package com.project.likelion13thbe.domain.product.exception;
+
+import com.project.likelion13thbe.global.apiPayload.exception.CustomException;
+
+public class ProductException extends CustomException {
+  public ProductException(ProductErrorCode errorCode) {
+    super(errorCode);
+  }
+}
