c4246d81c526317d2585491d5bcc2d355c45128a
seoaaa <seoaa3331@gmail.com>
2025-05-13 19:38:50 +0900

    ✨feat: 예외처리 구현

---
+++ src/main/java/com/project/likelion13thbe/domain/member/exception/MemberErrorCode.java
@@ -0,0 +1,16 @@
+package com.project.likelion13thbe.domain.member.exception;
+
+import com.project.likelion13thbe.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorCode implements BaseErrorCode {
+    MEMBER_ERROR_CODE(HttpStatus.NOT_FOUND,"MEMBER404_1","회원을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
