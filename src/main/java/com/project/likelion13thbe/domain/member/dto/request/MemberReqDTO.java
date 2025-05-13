148ffe020c2b8454d33e684cc37d117effed92ba
tuna1025 <jo215098@gmail.com>
2025-05-13 12:56:56 +0900

    ✨feat: MemberCreateReqDTO 유효성검사 추가

--- src/main/java/com/project/likelion13thbe/domain/member/dto/request/MemberReqDTO.java
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/request/MemberReqDTO.java
@@ -2,6 +2,7 @@ package com.project.likelion13thbe.domain.member.dto.request;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -10,9 +11,18 @@ public class MemberReqDTO {
 
     @Builder
     public record MemberCreateReqDTO(
+            @NotBlank(message = "이름은 필수 입력 항목입니다.")
+            @Size(min = 2, max = 50, message = "이름은 2자 이상, 50자 이하로 입력해주세요.")
             String name,
+
+            @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+            @Email(message = "올바른 이메일 형식을 입력해주세요.")
             String email,
+
+            @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+            @Size(min = 8, message = "비밀번호는 최소 8자 이상 입력해야 합니다.")
             String password
+
     ){
     }
     //비밀번호 업데이트 Res
