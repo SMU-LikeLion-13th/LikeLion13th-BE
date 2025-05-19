257df9c00df0272f151e9d92a04c61e38bed8e9b
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-05-20 08:31:10 +0900

    ✨feat: AuthException and AuthErrorCode

---
+++ src/main/java/com/project/likelion13thbe/global/security/exception/AuthErrorCode.java
@@ -0,0 +1,16 @@
+package com.project.likelion13thbe.global.security.exception;
+
+import com.project.likelion13thbe.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AuthErrorCode implements BaseErrorCode {
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH401_1", "토큰이 유효하지 않습니다."),;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/com/project/likelion13thbe/global/security/exception/AuthException.java
@@ -0,0 +1,10 @@
+package com.project.likelion13thbe.global.security.exception;
+
+import com.project.likelion13thbe.global.apiPayload.exception.CustomException;
+
+public class AuthException extends CustomException {
+
+    public AuthException(AuthErrorCode errorCode) {
+        super(errorCode);
+    }
+}
