bf2e39db801c98fd4b672d82262278ca99f7c2d1
Gyueon Lee <gon2yq@naver.com>
2025-05-19 17:48:16 +0900

    ♻️refactor: 표기 변경

--- src/main/java/com/project/likelion13thbe/global/security/customUserDetails/CustomUserDetails.java
+++ src/main/java/com/project/likelion13thbe/global/security/customUserDetails/CustomUserDetails.java
@@ -25,7 +25,7 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(String.valueOf(roles)));
+        authorities.add(new SimpleGrantedAuthority(roles.name()));
         return authorities;
     }
 
