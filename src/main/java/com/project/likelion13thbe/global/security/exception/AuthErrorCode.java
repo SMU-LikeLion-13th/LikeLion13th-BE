014e8306b330f8b0a0a422fa7f81eaed0d6c125f
Gyueon Lee <gon2yq@naver.com>
2025-05-19 14:56:54 +0900

    ✨feat: AuthErrorCode & Exception 추가 (실습)

---
+++ src/main/java/com/project/likelion13thbe/global/security/exception/AuthErrorCode.java
@@ -0,0 +1,21 @@
+package com.project.likelion13thbe.global.security.exception;
+
+import com.project.likelion13thbe.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum AuthErrorCode implements BaseErrorCode {
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다"),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "접근이 금지되었습니다"),
+    _NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404", "요청한 자원을 찾을 수 없습니다"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "COMMON401", "이상한 토큰입니다")
+    ;
+
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
