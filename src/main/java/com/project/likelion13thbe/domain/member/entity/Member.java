09045f970c7be0bdf26f156909c99903ba3c20b5
Hyunwoo Choi <choihw1230@naver.com>
2025-05-20 20:46:13 +0900

    ✨feat: Member의 Role 기본값 설정

--- src/main/java/com/project/likelion13thbe/domain/member/entity/Member.java
+++ src/main/java/com/project/likelion13thbe/domain/member/entity/Member.java
@@ -37,9 +37,16 @@ public class Member extends BaseEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
-    @Column(name = "role")
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @PrePersist
+    public void setDefaultRole() {
+        if (this.role == null) {
+            this.role = Role.USER;
+        }
+    }
 
     // 비밀번호 변경 메서드
     public void updatePassword(String newPassword) { this.password = newPassword; }
