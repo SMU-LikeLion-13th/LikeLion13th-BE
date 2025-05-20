6e6c519e5cd99632236a63765a941d436f1b5f87
Hyunwoo Choi <choihw1230@naver.com>
2025-05-20 10:39:14 +0900

    ✨feat: SecurityConfig 추가

--- src/main/java/com/project/likelion13thbe/global/Security/SecurityConfig.java
+++ src/main/java/com/project/likelion13thbe/global/Security/SecurityConfig.java
@@ -44,19 +44,29 @@ public class SecurityConfig {
         loginFilter.setFilterProcessesUrl("/api/v1/login");
 
         http
+                // 허용할 URL, 역할별로 나눌 URL, 인증을 요구하는 URL 설절
                 .authorizeHttpRequests(request -> request
+                        // allowUrl 모두 허용
                         .requestMatchers(allowUrl).permitAll()
+                        // 이외의 요청에 대해서 인증이 필요하도록 설정
                         .anyRequest().authenticated())
+                // jwtFilter를 UsernaemPasswordAuthenticationFilter 앞에 오도록 설절
                 .addFilterBefore(new JwtAuthorizationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
+                // formLogic 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)
+                // httpBasic 비활성화
                 .httpBasic(HttpBasicConfigurer::disable)
+                // csrf 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
+                // 인증 인가에 대한 예외처리
                 .exceptionHandling(exceptionHandling -> exceptionHandling
+                        // 인가에 대해 예외처리할 핸들러 추가
                         .accessDeniedHandler(jwtAccessDeniedHandler)
+                        // 인증에 대해 예외처리할 핸들러 추가
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint))
         ;
-
+        // build 해서 시큐리티필터체인 현태로 반환
         return http.build();
     }
 
