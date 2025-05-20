e05ee74acf4a2704ea4c7b41a4bc0d93f102bd0b
Hyunwoo Choi <choihw1230@naver.com>
2025-05-20 10:04:08 +0900

    ✨feat: Token DTO 추가

---
+++ src/main/java/com/project/likelion13thbe/global/Security/DTO/JwtDTO.java
@@ -0,0 +1,16 @@
+package com.project.likelion13thbe.global.Security.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtDTO {
+    // 로그인 성공 -> 토큰 응답
+    public String accessToken; // 엑세스 토큰
+    public String refreshToken; // 리프레쉬 토큰
+}
