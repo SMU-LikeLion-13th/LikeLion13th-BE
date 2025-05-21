615a093984a05bdb4503ac98c1eb5ade08cfa6a3
hanmin <rlemdhs2747@naver.com>
2025-05-21 19:21:49 +0900

    KakaoTokenResDTO, KakaoUserInfoResDTO 추가

---
+++ src/main/java/com/project/likelion13thbe/domain/member/dto/response/KakaoTokenResDTO.java
@@ -0,0 +1,24 @@
+package com.project.likelion13thbe.domain.member.dto.response;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record KakaoTokenResDTO (
+        @JsonProperty("token_type")
+        String tokenType,
+        @JsonProperty("access_token")
+        String accessToken,
+        @JsonProperty("id_token")
+        String idToken,
+        @JsonProperty("expires_in")
+        Integer expiresIn,
+        @JsonProperty("refresh_token")
+        String refreshToken,
+        @JsonProperty("refresh_token_expires_in")
+        Integer refreshTokenExpiresIn,
+        @JsonProperty("scope")
+        String scope
+) {}
+
