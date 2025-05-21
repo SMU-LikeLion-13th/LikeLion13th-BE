fc79ec9e3d204381b3216d1982455df1d4d8685d
seoaaa <seoaa3331@gmail.com>
2025-05-21 12:49:37 +0900

    ✨️feat : Auth 관련 구현

---
+++ src/main/java/com/project/likelion13thbe/global/security/exception/AuthException.java
@@ -0,0 +1,11 @@
+package com.project.likelion13thbe.global.security.exception;
+
+import com.project.likelion13thbe.global.apiPayload.exception.CustomException;
+import lombok.Getter;
+
+@Getter
+public class AuthException extends CustomException {
+    public AuthException(AuthErrorCode errorCode) {
+        super(errorCode);
+    }
+}
