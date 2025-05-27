8fad5c4ccbf12ad799364d9469f7c3d3d996c851
Gyueon Lee <gon2yq@naver.com>
2025-05-27 13:04:52 +0900

    ✨feat: @Operation 추가

--- src/main/java/com/project/likelion13thbe/global/S3/S3Controller.java
+++ src/main/java/com/project/likelion13thbe/global/S3/S3Controller.java
@@ -1,6 +1,7 @@
 package com.project.likelion13thbe.global.S3;
 
 import com.project.likelion13thbe.global.apiPayload.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class S3Controller {
     private final S3PresignedService s3PresignedService;
 
     // Presigned PUT URL 생성
+    @Operation(summary = "업로드 전용 URL 생성")
     @GetMapping("/upload-url")
     public CustomResponse<String> getUploadUrl(@RequestParam String fileName) {
         String url = s3PresignedService.generatePresignedPutUrl(fileName);
@@ -24,6 +26,7 @@ public class S3Controller {
     }
 
     // Presigned GET URL 생성
+    @Operation(summary = "다운로드 전용 URL 생성")
     @GetMapping("/download-url")
     public CustomResponse<String> getDownloadUrl(@RequestParam String fileName) {
         String url = s3PresignedService.generatePresignedGetUrl(fileName);
