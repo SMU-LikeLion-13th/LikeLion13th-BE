558c572d8d3b7ba103d4b633aa9f7b207c00d1df
Hyunwoo Choi <choihw1230@naver.com>
2025-05-27 09:31:57 +0900

    🚀chore: 패키지 위치 변경

--- src/main/java/com/project/likelion13thbe/global/config/SecurityConfig.java
+++ src/main/java/com/project/likelion13thbe/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.project.likelion13thbe.global.Security;
+package com.project.likelion13thbe.global.config;
 
+import com.project.likelion13thbe.global.Security.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
