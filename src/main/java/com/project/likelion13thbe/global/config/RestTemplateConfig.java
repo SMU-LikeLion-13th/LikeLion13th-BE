1e5da929d708c1a2e4847f0b7354eddbad5a2d02
hanmin <rlemdhs2747@naver.com>
2025-05-27 20:19:17 +0900

    RedisTemplateConfig 추가

---
+++ src/main/java/com/project/likelion13thbe/global/config/RestTemplateConfig.java
@@ -0,0 +1,26 @@
+package com.project.likelion13thbe.global.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class RestTemplateConfig {
+
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .requestFactory(
+                        () -> new BufferingClientHttpRequestFactory(
+                                new SimpleClientHttpRequestFactory()
+                        )
+                        ).additionalMessageConverters(
+                                new StringHttpMessageConverter(
+                                        StandardCharsets.UTF_8
+                                )).build();
+    }
+}
