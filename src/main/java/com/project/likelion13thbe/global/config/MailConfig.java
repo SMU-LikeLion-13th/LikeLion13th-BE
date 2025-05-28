1a6610a6e9110eed5350e946e8d941db97518671
Hyunwoo Choi <choihw1230@naver.com>
2025-05-28 17:56:14 +0900

    ✨feat: SMTP 구현, @value를 통해서 환경 변수 설정 가져오기

--- src/main/java/com/project/likelion13thbe/global/config/MailConfig.java
+++ src/main/java/com/project/likelion13thbe/global/config/MailConfig.java
@@ -1,26 +1,38 @@
 package com.project.likelion13thbe.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
 import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+
+    private final String username;
+    private final String pw;
+
+    public MailConfig(
+            @Value("${spring.mail.username}") String username,
+            @Value("${spring.mail.password}") String password ){
+        this.username = username;
+        this.pw = password;
+    }
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("설정 한 이메일 주소");
-        mailSender.setPassword("발급 받은 비밀번호");
+        mailSender.setUsername(username);
+        mailSender.setPassword(pw);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
         props.put("mail.debug", "true");
 
         return mailSender;
