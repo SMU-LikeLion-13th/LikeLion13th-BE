a59d0fb25e2172525fca7ca2f56edf3e8afd1b40
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-05-30 18:51:34 +0900

    first commit

---
+++ src/main/java/com/example/redisspring/BoardService.java
@@ -0,0 +1,26 @@
+package com.example.redisspring;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    // cache aside 전략으로 캐싱이 적용 된다.
+    // 알아서 캐시 찌르고 없으면 알아서 다시 DB로 간 다음 저장시켜준다
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
+    public List<Board> getBoards(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return pageOfBoards.getContent();
+    }
+}
